ea02c21efcfb8af01232bccf85d3ac6b97168af1
getsoss <getsoss@naver.com>
2025-02-22 22:04:58 +0900

    [chore] 프리즈마 스키마 이슈 주석

--- application/usecases/GetSpotUseCase.ts
+++ application/usecases/GetSpotUseCase.ts
@@ -3,6 +3,6 @@ import { Spot } from "@/domain/entities/Spot";
 
 export class GetSpotUseCase {
   async execute(): Promise<Spot[]> {
-    return await prisma.spot.findMany();
+    return await prisma.spot.findMany(); // 프리즈마 스키마 파일과 형태가 달라서 에러 메세지 발생
   }
 }
