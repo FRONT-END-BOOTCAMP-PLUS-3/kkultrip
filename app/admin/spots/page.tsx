58349c5d414f6dc0b1c06c2ee001d60e391b64d1
getsoss <getsoss@naver.com>
2025-02-22 21:32:10 +0900

    [feat] prisma 테스트 데이터 조회

--- app/admin/spots/page.tsx
+++ app/admin/spots/page.tsx
@@ -3,8 +3,12 @@ import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import UserTable from "../components/UserTable";
 import styles from "./AdminSpotsPage.module.scss";
+import { prisma } from "@/lib/prisma";
 
-const AdminSpotsPage = () => {
+const AdminSpotsPage = async () => {
+  const spots = await prisma.spot.findMany(); // Spot 테이블 조회
+  console.log("Spot 데이터:", spots); // 콘솔에 데이터 출력
+
   return (
     <div className={styles.container}>
       <Sidebar />
