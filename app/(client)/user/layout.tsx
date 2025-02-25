8d56b6ad3a13e0b3dc11d49dbdf61eb22ffbfb8c
sojeong <sojeong9432@gmail.com>
2025-02-25 15:50:23 +0900

    [style] user페이지 layout UI 구현

---
+++ app/(client)/user/layout.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import styles from "./userLayout.module.scss";
+import Tab from "./components/Tab";
+import { usePathname } from "next/navigation";
+
+const UserLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const tabs = [
+    { name: "나의 꿀팁", path: "/user/my-tips" },
+    { name: "관심 명소", path: "/user/spots" },
+    { name: "관심 꿀팁", path: "/user/tips" },
+  ];
+
+  return (
+    <div className={styles.userContainer}>
+      <div className={styles.profile}>
+        <Image
+          src="/images/bee.svg"
+          width={160}
+          height={160}
+          alt="profile image"
+          style={{ objectFit: "contain", width: "70%", height: "70%" }}
+        />
+      </div>
+      <div className={styles.nickname}>
+        <span>고독한 미식가</span>
+      </div>
+      <Tab tabs={tabs} pathname={pathname} />
+      {children}
+    </div>
+  );
+};
+
+export default UserLayout;
