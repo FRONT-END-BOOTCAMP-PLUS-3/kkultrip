a5559a1671e8d759c8763c8bc02ca0932406cf0f
whddbsl <whddbs04140@naver.com>
2025-02-25 23:42:27 +0900

    [style] spots 상세 페이지 - 사진

--- app/(client)/(anon)/spots/[spotId]/images/page.tsx
+++ app/(client)/(anon)/spots/[spotId]/images/page.tsx
@@ -1,5 +1,14 @@
+import SpotImage from "./components/SpotImage";
+import styles from "./imagePage.module.scss";
+
 const ImagesPage = () => {
-  return <div>ImagesPage</div>;
+  return <div className={styles.imageContainer}>
+    <SpotImage />
+    <SpotImage />
+    <SpotImage />
+    <SpotImage />
+    <SpotImage />
+  </div>;
 };
 
 export default ImagesPage;
