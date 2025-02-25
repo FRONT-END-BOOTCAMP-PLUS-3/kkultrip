540d0bac2af07eafb43b64e19f7971549468e292
whddbsl <whddbs04140@naver.com>
2025-02-25 23:42:53 +0900

    [style] spots 상세 페이지 - 꿀팁

--- app/(client)/(anon)/spots/[spotId]/tips/page.tsx
+++ app/(client)/(anon)/spots/[spotId]/tips/page.tsx
@@ -1,5 +1,67 @@
-const TipsPage = () => {
-  return <div>TipsPage</div>;
+import Link from "next/link";
+import Tip from "./components/Tip";
+import styles from "./tipsPage.module.scss";
+import Reaction from "./components/Reaction";
+
+const TipsPage = async ({ params }: { params: { spotId: string } }) => {
+    const { spotId } = params;
+
+    return (
+        <div className={styles.tipsContainer}>
+            <h2 className={styles.srOnly}>불국사 꿀팁</h2>
+            <ul className={styles.sortContainer}>
+                <li>
+                    <button>
+                        <Link
+                            href={`/spots/${spotId}/tips?sort=latest`}
+                            className={styles.link}
+                        >
+                            • 최신순
+                        </Link>
+                    </button>
+                </li>
+                <li>
+                    <button>
+                        <Link
+                            href={`/spots/${spotId}/tips?sort=reaction`}
+                            className={styles.link}
+                        >
+                            • 반응순
+                        </Link>
+                    </button>
+                </li>
+            </ul>
+            
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+            <div className={styles.tipContainer}>
+                <Tip />
+                <Reaction />
+            </div>
+        </div>
+    );
 };
 
 export default TipsPage;
