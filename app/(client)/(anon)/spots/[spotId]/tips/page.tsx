5555366c9d1b9bb638d49121fb1c0413849997ec
Kim Jong Yun <130979302+whddbsl@users.noreply.github.com>
2025-02-28 15:42:33 +0900

    [feat]  상세 페이지 꿀팁 최신순, 반응순에 따른 하이라이트 추가

--- app/(client)/(anon)/spots/[spotId]/tips/page.tsx
+++ app/(client)/(anon)/spots/[spotId]/tips/page.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import Reaction from "./components/Reaction";
 import Tip from "./components/Tip";
 import styles from "./tipsPage.module.scss";
-import Reaction from "./components/Reaction";
 
-const TipsPage = async ({ params }: { params: { spotId: string } }) => {
+const TipsPage = ({
+    params,
+    searchParams,
+}: {
+    params: { spotId: string };
+    searchParams: { sort?: string };
+}) => {
     const { spotId } = params;
-
+    const sort = searchParams.sort || "latest";
+    
     return (
         <div className={styles.tipsContainer}>
             <h2 className={styles.srOnly}>불국사 꿀팁</h2>
@@ -14,7 +21,9 @@ const TipsPage = async ({ params }: { params: { spotId: string } }) => {
                     <button>
                         <Link
                             href={`/spots/${spotId}/tips?sort=latest`}
-                            className={styles.link}
+                            className={`${styles.link} ${
+                                sort === "latest" ? styles.activeLink : ""
+                            }`}
                         >
                             • 최신순
                         </Link>
@@ -24,14 +33,14 @@ const TipsPage = async ({ params }: { params: { spotId: string } }) => {
                     <button>
                         <Link
                             href={`/spots/${spotId}/tips?sort=reaction`}
-                            className={styles.link}
+                            className={`${styles.link} ${sort === "reaction" ? styles.activeLink : ""}`}
                         >
                             • 반응순
                         </Link>
                     </button>
                 </li>
             </ul>
-            
+
             <div className={styles.tipContainer}>
                 <Tip />
                 <Reaction />
