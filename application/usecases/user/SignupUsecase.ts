f135799f04ba68cc114be963efb0e52909303e5a
parksojeong <sojeong9432@gmail.com>
2025-03-04 02:01:56 +0900

    [feat] 비밀번호 암호화 추가

--- application/usecases/user/SignupUsecase.ts
+++ application/usecases/user/SignupUsecase.ts
@@ -1,18 +1,20 @@
 import { UserRepository } from "@/domain/repositories/UserRepository";
 import { User } from "@prisma/client";
 import { CreateUserDto } from "./dto/CreateUserDto";
+import bcrypt from "bcrypt";
 
 export default class SignupUsecase {
   constructor(private repository: UserRepository) {}
 
   async execute(data: CreateUserDto): Promise<User> {
     const { img, nickname, email, password } = data;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user: CreateUserDto = {
       img: img,
       nickname: nickname,
       email: email,
-      password: password,
+      password: hashedPassword,
     };
 
     const newUser = await this.repository.createUser(user);
