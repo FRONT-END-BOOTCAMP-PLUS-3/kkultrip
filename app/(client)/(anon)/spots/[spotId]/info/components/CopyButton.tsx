646003338aa7873e4d3dfd9eab39a56856abd314
Kim Jong Yun <130979302+whddbsl@users.noreply.github.com>
2025-03-03 15:53:44 +0900

    [fix] 모바일웹에서 복사 기능 동작 오류 해결

--- app/(client)/(anon)/spots/[spotId]/info/components/CopyButton.tsx
+++ app/(client)/(anon)/spots/[spotId]/info/components/CopyButton.tsx
@@ -1,3 +1,37 @@
+// "use client";
+
+// import styles from "./copyButton.module.scss";
+// import { useState } from "react";
+
+// const CopyButton = ({ text }: { text: string }) => {
+//     const [showMessage, setShowMessage] = useState(false);
+
+//     const handleCopyPhone = () => {
+//         navigator.clipboard
+//             .writeText(text)
+//             .then(() => {
+//                 setShowMessage(true);
+//                 setTimeout(() => setShowMessage(false), 1000);
+//             })
+//             .catch((err) => {
+//                 console.error("복사 실패: ", err);
+//             });
+//     };
+
+//     return (
+//         <div className={styles.copyButtonContainer}>
+//             <button className={styles.copyButton} onClick={handleCopyPhone}>
+//                 복사
+//             </button>
+//             {showMessage && (
+//                 <div className={styles.copyMessage}>복사되었습니다!</div>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default CopyButton;
+
 "use client";
 
 import styles from "./copyButton.module.scss";
@@ -7,15 +41,31 @@ const CopyButton = ({ text }: { text: string }) => {
     const [showMessage, setShowMessage] = useState(false);
 
     const handleCopyPhone = () => {
-        navigator.clipboard
-            .writeText(text)
-            .then(() => {
+        if (navigator.clipboard) {
+            navigator.clipboard
+                .writeText(text)
+                .then(() => {
+                    setShowMessage(true);
+                    setTimeout(() => setShowMessage(false), 1000);
+                })
+                .catch((err) => {
+                    console.error("복사 실패: ", err);
+                });
+        } else {
+            // Fallback for older browsers
+            const textArea = document.createElement("textarea");
+            textArea.value = text;
+            document.body.appendChild(textArea);
+            textArea.select();
+            try {
+                document.execCommand("copy");
                 setShowMessage(true);
-                setTimeout(() => setShowMessage(false), 1000); 
-            })
-            .catch((err) => {
+                setTimeout(() => setShowMessage(false), 1000);
+            } catch (err) {
                 console.error("복사 실패: ", err);
-            });
+            }
+            document.body.removeChild(textArea);
+        }
     };
 
     return (
