a641ba6ce62f2468947760ae3de1d91756117a4e
parksojeong <sojeong9432@gmail.com>
2025-03-08 13:22:29 +0900

    [feat] zustand에 있는 img, nickname 가져와서 렌더링하기

--- app/(client)/user/components/UserProfile.tsx
+++ app/(client)/user/components/UserProfile.tsx
@@ -2,20 +2,23 @@
 
 import Image from "next/image";
 import styles from "./UserProfile.module.scss";
+import useUserStore from "@/store/useUserStore";
 
 const UserProfile = () => {
+  const { img, nickname } = useUserStore();
+
   return (
     <div className={styles.profileContainer}>
       <div className={styles.profile}>
         <Image
-          src="/images/test.png"
+          src={img || "/images/default.png"}
           fill
           alt="profile image"
           className={styles.profileImage}
         />
       </div>
       <div className={styles.nickname}>
-        <span>고독한 미식가</span>
+        <span>{nickname}</span>
       </div>
     </div>
   );
