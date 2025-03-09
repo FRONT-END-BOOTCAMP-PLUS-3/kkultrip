1979d5bd5c7c0ce8db7d13c09f5b69d8dd09b372
parksojeong <sojeong9432@gmail.com>
2025-03-09 14:45:49 +0900

    [fix] 쿠키만료방식으로 쿠키 삭제했던 코드 쿠키 삭제방식으로 변경

--- app/api/withdraw/route.ts
+++ app/api/withdraw/route.ts
@@ -33,13 +33,7 @@ export async function POST(req: NextRequest) {
       { status: 200 }
     );
 
-    response.cookies.set("token", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      expires: new Date(0),
-      path: "/",
-    });
+    response.cookies.delete("token");
 
     return response;
   } catch (error: unknown) {
