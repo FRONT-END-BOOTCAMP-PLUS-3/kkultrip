97d69066a30ff0fc2f87b42a00b2007a0f954e54
getsoss <getsoss@naver.com>
2025-03-11 18:21:33 +0900

    [fix] 관리자페이지/꿀팁검색 유저 검색시 부분검색

--- application/usecases/admin/tip/GetTipByUserNameUseCase.ts
+++ application/usecases/admin/tip/GetTipByUserNameUseCase.ts
@@ -23,34 +23,41 @@ export default class GetTipByUserNameUseCase {
     orderBy: "createdAt" | "reactionCount"
   ): Promise<GetTipListDto[]> {
     try {
-      // 사용자 이름으로 사용자 정보를 가져옴
-      const user = await this.userRepository.getUserByName(userName);
+      // 사용자 이름으로 사용자 목록을 가져옴
+      const users = await this.userRepository.getUsersByPartialName(userName);
 
-      if (!user) {
+      if (users!.length === 0) {
         throw new Error("사용자를 찾을 수 없습니다.");
       }
 
-      // 사용자의 팁들을 가져옴
-      const tips = await this.tipRepository.getTipsByUserId(user.id);
+      // 각 사용자의 팁들을 가져옴
+      const tipsWithSpotInfo: GetTipListDto[] = [];
 
-      // 팁에 대한 장소 정보를 추가
-      const tipsWithSpotInfo = await Promise.all(
-        tips.map(async (tip) => {
-          const spot = await this.spotRepository.getSpotById(tip.spotId);
-          return {
-            id: tip.id,
-            spotId: tip.spotId,
-            spotName: spot!.name, // spotName을 장소에서 가져옵니다.
-            nickname: user.nickname, // 유저의 nickname을 가져옵니다.
-            description: tip.description,
-            price: tip.price,
-            waitingTime: tip.waitingTime,
-            reportCnt: tip.reportCnt,
-            createdAt: tip.createdAt,
-            updatedAt: tip.updatedAt,
-          };
-        })
-      );
+      for (const user of users!) {
+        const tips = await this.tipRepository.getTipsByUserId(user.id);
+
+        // 팁에 대한 장소 정보를 추가
+        const userTipsWithSpotInfo = await Promise.all(
+          tips.map(async (tip) => {
+            const spot = await this.spotRepository.getSpotById(tip.spotId);
+            return {
+              id: tip.id,
+              spotId: tip.spotId,
+              spotName: spot!.name, // spotName을 장소에서 가져옵니다.
+              nickname: user.nickname, // 유저의 nickname을 가져옵니다.
+              description: tip.description,
+              price: tip.price,
+              waitingTime: tip.waitingTime,
+              reportCnt: tip.reportCnt,
+              createdAt: tip.createdAt,
+              updatedAt: tip.updatedAt,
+            };
+          })
+        );
+
+        // 해당 사용자의 팁 정보를 추가
+        tipsWithSpotInfo.push(...userTipsWithSpotInfo);
+      }
 
       return tipsWithSpotInfo;
     } catch (error) {
