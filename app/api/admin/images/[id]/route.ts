2739fe84162bdd45cabb86c7804326ac950e8b88
getsoss <getsoss@naver.com>
2025-03-11 20:21:37 +0900

    [fix] 이미지 삭제로직

--- app/api/admin/images/[id]/route.ts
+++ app/api/admin/images/[id]/route.ts
@@ -1,31 +1,29 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import DeleteImageUseCase from "@/application/usecases/spot/images/DeleteImageUseCase";
 import { PgImageRepository } from "@/infrastructure/repositories/PgImageRepository";
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const imageRepository = new PgImageRepository();
-  const deleteImageUseCase = new DeleteImageUseCase(imageRepository);
+export async function DELETE(req: NextRequest) {
   try {
-    const id = Number(params.id); // id를 숫자로 변환
+    const pathname = req.nextUrl.pathname;
+    const imageId = pathname.split("/").pop(); // Extract image ID from the URL
 
-    if (isNaN(id)) {
+    if (!imageId || isNaN(Number(imageId))) {
       return NextResponse.json(
         { error: "유효하지 않은 이미지 ID입니다." },
         { status: 400 }
       );
     }
 
-    await deleteImageUseCase.execute(id);
+    const imageRepository = new PgImageRepository();
+    const deleteImageUseCase = new DeleteImageUseCase(imageRepository);
+    await deleteImageUseCase.execute(Number(imageId));
 
     return NextResponse.json(
       { message: "이미지가 성공적으로 삭제되었습니다." },
       { status: 200 }
     );
   } catch (error) {
-    console.error(error);
+    console.error("Error deleting image:", error);
     return NextResponse.json(
       { error: "이미지 삭제에 실패했습니다." },
       { status: 500 }
