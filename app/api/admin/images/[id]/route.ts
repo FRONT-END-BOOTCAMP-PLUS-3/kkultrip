cbd033e3e24fbb8c6aaaba4ad344d85e7c85ba30
getsoss <getsoss@naver.com>
2025-03-11 20:12:56 +0900

    [fix] API

--- app/api/admin/images/[id]/route.ts
+++ app/api/admin/images/[id]/route.ts
@@ -9,18 +9,25 @@ export async function DELETE(
   const imageRepository = new PgImageRepository();
   const deleteImageUseCase = new DeleteImageUseCase(imageRepository);
   try {
-    const id = params.id;
+    const id = Number(params.id); // id를 숫자로 변환
 
-    await deleteImageUseCase.execute(Number(id));
+    if (isNaN(id)) {
+      return NextResponse.json(
+        { error: "유효하지 않은 이미지 ID입니다." },
+        { status: 400 }
+      );
+    }
+
+    await deleteImageUseCase.execute(id);
 
     return NextResponse.json(
-      { message: "Image deleted successfully" },
+      { message: "이미지가 성공적으로 삭제되었습니다." },
       { status: 200 }
     );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "Failed to delete image" },
+      { error: "이미지 삭제에 실패했습니다." },
       { status: 500 }
     );
   }
