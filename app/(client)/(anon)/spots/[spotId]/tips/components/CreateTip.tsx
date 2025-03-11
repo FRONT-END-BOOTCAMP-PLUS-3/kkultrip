04bf96808e660d74a132a5264d94549cb35f2b2b
sojeong <sojeong9432@gmail.com>
2025-03-11 16:21:45 +0900

    [chore] 주석 제거

--- app/(client)/(anon)/spots/[spotId]/tips/components/CreateTip.tsx
+++ app/(client)/(anon)/spots/[spotId]/tips/components/CreateTip.tsx
@@ -2,19 +2,10 @@
 
 import { FaPlus } from "react-icons/fa";
 import styles from "./CreateTip.module.scss";
-// import useUserStore from "@/store/useUserStore";
-// import { useRouter } from "next/navigation";
 
 const CreateTip = ({ spotId }: { spotId: string }) => {
-  // const router = useRouter();
-  // const isLoggedIn = useUserStore((state) => state.isLoggedIn);
-
   const handleClick = () => {
-    // if (isLoggedIn) {
     window.location.href = `/spots/${spotId}/tips/create`;
-    // } else {
-    //     router.push("/login");
-    // }
   };
 
   return (
