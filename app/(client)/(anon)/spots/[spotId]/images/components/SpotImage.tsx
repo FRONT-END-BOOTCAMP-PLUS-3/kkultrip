73435bcd3234a232cf94f5febb26a2f97661cd01
Kim Jong Yun <130979302+whddbsl@users.noreply.github.com>
2025-03-12 21:57:14 +0900

    [fix] 상세페이지 이미지 url 수정

--- app/(client)/(anon)/spots/[spotId]/images/components/SpotImage.tsx
+++ app/(client)/(anon)/spots/[spotId]/images/components/SpotImage.tsx
@@ -13,11 +13,12 @@ const SpotImage = ({
     return (
         <Link href={`/spots/${spotId}/tips?sort=latest#${image.tipId}`}>
             <Image
-                src={image.path}
+                src={`${process.env.SERVICE_URL}${image.path}`}
                 alt="image"
                 width={100}
                 height={100}
                 className={styles.spotImage}
+                unoptimized
             />
         </Link>
     );
