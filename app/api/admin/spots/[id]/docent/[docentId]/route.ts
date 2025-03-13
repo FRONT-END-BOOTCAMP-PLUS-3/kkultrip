0ae6e4453bf7e07d3a9cf4cf518fafdfe41305cc
getsoss <getsoss@naver.com>
2025-03-13 10:12:03 +0900

    [feat] 도슨트 단일 삭제시 오디오 파일 삭제

--- app/api/admin/spots/[id]/docent/[docentId]/route.ts
+++ app/api/admin/spots/[id]/docent/[docentId]/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from "next/server";
 import PgDocentRepository from "@/infrastructure/repositories/PgDocentRepository";
-import { DeleteDocentUseCase } from "@/application/usecases/admin/spot/docent/dto/DeleteDocentUseCase";
+import { DeleteDocentUseCase } from "@/application/usecases/admin/spot/docent/DeleteDocentUseCase";
 import DocentRepository from "@/domain/repositories/DocentRepository";
+import { promises as fs } from "fs";
+import path from "path";
 
 export async function DELETE(req: Request) {
   try {
@@ -16,6 +18,32 @@ export async function DELETE(req: Request) {
     }
 
     const docentRepository: DocentRepository = new PgDocentRepository();
+
+    // 📌 도슨트 정보 가져오기
+    const docent = await docentRepository.getDocentById(Number(id));
+    if (!docent) {
+      return NextResponse.json({ error: "Docent not found" }, { status: 404 });
+    }
+
+    // 📌 오디오 파일 삭제
+    if (docent.audioPath) {
+      try {
+        const audioFilename = path.basename(docent.audioPath);
+        const audioPath = path.join(
+          process.cwd(),
+          "public",
+          "audios",
+          audioFilename
+        );
+        await fs.access(audioPath);
+        await fs.unlink(audioPath);
+        console.log(`Deleted audio file: ${audioFilename}`);
+      } catch (error) {
+        console.log("No existing audio file found or failed to delete:", error);
+      }
+    }
+
+    // 📌 도슨트 삭제
     const deleteDocentUseCase = new DeleteDocentUseCase(docentRepository);
     await deleteDocentUseCase.execute(Number(id));
 
